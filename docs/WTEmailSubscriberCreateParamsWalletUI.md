f82be04a024736b539af7d2c162c875ba285bfe4
johnturnham <support@wallet.inc>
2023-05-31 23:39:39 +0000

    v2.1.535

---
+++ docs/WTEmailSubscriberCreateParamsWalletUI.md
@@ -0,0 +1,14 @@
+# WTEmailSubscriberCreateParamsWalletUI
+
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**first_name** | **str** |  | 
+**last_name** | **str** |  | 
+**email_address** | **str** |  | 
+**merchant_id** | [**MerchantID**](MerchantID.md) |  | 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
--- wallet/api/interactions_api.py
+++ wallet/api/interactions_api.py
@@ -25,12 +25,14 @@ from wallet.model_utils import (  # noqa: F401
 from wallet.model.advertisement_credit_scan import AdvertisementCreditScan
 from wallet.model.browser_details import BrowserDetails
 from wallet.model.dynamic_voucher import DynamicVoucher
+from wallet.model.email_subscriber import EmailSubscriber
 from wallet.model.falsum_error import FalsumError
 from wallet.model.inline_response2009 import InlineResponse2009
 from wallet.model.internal_server_error import InternalServerError
 from wallet.model.member import Member
 from wallet.model.merchant_id import MerchantID
 from wallet.model.static_voucher import StaticVoucher
+from wallet.model.wt_email_subscriber_create_params_wallet_ui import WTEmailSubscriberCreateParamsWalletUI
 from wallet.model.wt_fetch_wallet_payment_object_with_token import WTFetchWalletPaymentObjectWithToken
 from wallet.model.wallet_configuration import WalletConfiguration
 
@@ -694,6 +696,56 @@ class InteractionsApi(object):
                 'location_map': {
                     'item_id': 'path',
                     'browser_details': 'body',
+                },
+                'collection_format_map': {
+                }
+            },
+            headers_map={
+                'accept': [
+                    'application/json'
+                ],
+                'content_type': [
+                    'application/json'
+                ]
+            },
+            api_client=api_client
+        )
+        self.subscribe_email_endpoint = _Endpoint(
+            settings={
+                'response_type': (EmailSubscriber,),
+                'auth': [],
+                'endpoint_path': '/wallet/subscribeEmail',
+                'operation_id': 'subscribe_email',
+                'http_method': 'POST',
+                'servers': None,
+            },
+            params_map={
+                'all': [
+                    'wt_email_subscriber_create_params_wallet_ui',
+                ],
+                'required': [
+                    'wt_email_subscriber_create_params_wallet_ui',
+                ],
+                'nullable': [
+                ],
+                'enum': [
+                ],
+                'validation': [
+                ]
+            },
+            root_map={
+                'validations': {
+                },
+                'allowed_values': {
+                },
+                'openapi_types': {
+                    'wt_email_subscriber_create_params_wallet_ui':
+                        (WTEmailSubscriberCreateParamsWalletUI,),
+                },
+                'attribute_map': {
+                },
+                'location_map': {
+                    'wt_email_subscriber_create_params_wallet_ui': 'body',
                 },
                 'collection_format_map': {
                 }
@@ -1719,4 +1771,81 @@ class InteractionsApi(object):
         kwargs['browser_details'] = \
             browser_details
         return self.request_merchant_url_redirect_endpoint.call_with_http_info(**kwargs)
+
+    def subscribe_email(
+        self,
+        wt_email_subscriber_create_params_wallet_ui,
+        **kwargs
+    ):
+        """Create email subscriber  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+
+        >>> thread = api.subscribe_email(wt_email_subscriber_create_params_wallet_ui, async_req=True)
+        >>> result = thread.get()
+
+        Args:
+            wt_email_subscriber_create_params_wallet_ui (WTEmailSubscriberCreateParamsWalletUI):
+
+        Keyword Args:
+            _return_http_data_only (bool): response data without head status
+                code and headers. Default is True.
+            _preload_content (bool): if False, the urllib3.HTTPResponse object
+                will be returned without reading/decoding response data.
+                Default is True.
+            _request_timeout (int/float/tuple): timeout setting for this request. If
+                one number provided, it will be total request timeout. It can also
+                be a pair (tuple) of (connection, read) timeouts.
+                Default is None.
+            _check_input_type (bool): specifies if type checking
+                should be done one the data sent to the server.
+                Default is True.
+            _check_return_type (bool): specifies if type checking
+                should be done one the data received from the server.
+                Default is True.
+            _spec_property_naming (bool): True if the variable names in the input data
+                are serialized names, as specified in the OpenAPI document.
+                False if the variable names in the input data
+                are pythonic names, e.g. snake case (default)
+            _content_type (str/None): force body content-type.
+                Default is None and content-type will be predicted by allowed
+                content-types and body.
+            _host_index (int/None): specifies the index of the server
+                that we want to use.
+                Default is read from the configuration.
+            async_req (bool): execute request asynchronously
+
+        Returns:
+            EmailSubscriber
+                If the method is called asynchronously, returns the request
+                thread.
+        """
+        kwargs['async_req'] = kwargs.get(
+            'async_req', False
+        )
+        kwargs['_return_http_data_only'] = kwargs.get(
+            '_return_http_data_only', True
+        )
+        kwargs['_preload_content'] = kwargs.get(
+            '_preload_content', True
+        )
+        kwargs['_request_timeout'] = kwargs.get(
+            '_request_timeout', None
+        )
+        kwargs['_check_input_type'] = kwargs.get(
+            '_check_input_type', True
+        )
+        kwargs['_check_return_type'] = kwargs.get(
+            '_check_return_type', True
+        )
+        kwargs['_spec_property_naming'] = kwargs.get(
+            '_spec_property_naming', False
+        )
+        kwargs['_content_type'] = kwargs.get(
+            '_content_type')
+        kwargs['_host_index'] = kwargs.get('_host_index')
+        kwargs['wt_email_subscriber_create_params_wallet_ui'] = \
+            wt_email_subscriber_create_params_wallet_ui
+        return self.subscribe_email_endpoint.call_with_http_info(**kwargs)
 
