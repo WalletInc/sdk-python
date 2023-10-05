c2d7610c000c19fe07eb17dd94075b132cc81f84
johnturnham <support@wallet.inc>
2023-10-05 19:18:34 +0000

    v2.1.535

--- docs/InlineObject1.md
+++ docs/InlineObject1.md
@@ -7,6 +7,7 @@ Name | Type | Description | Notes
 **ticket_expiration_date_time** | **datetime** |  | 
 **max_comp_tickets** | **float** |  | 
 **payment_design_id** | **bool, date, datetime, dict, float, int, list, str, none_type** |  | 
+**redemption_instructions** | **str** |  | [optional] 
 **any string name** | **bool, date, datetime, dict, float, int, list, str, none_type** | any string name can be used but the value must be the correct type | [optional]
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- wallet/model/performance.py
+++ wallet/model/performance.py
@@ -110,6 +110,7 @@ class Performance(ModelNormal):
             'payment_design_id': (NanoID,),  # noqa: E501
             'max_comp_tickets': (float,),  # noqa: E501
             'ticket_expiration_date_time': (datetime,),  # noqa: E501
+            'redemption_instructions': (str,),  # noqa: E501
         }
 
     @cached_property
@@ -134,6 +135,7 @@ class Performance(ModelNormal):
         'payment_design_id': 'paymentDesignID',  # noqa: E501
         'max_comp_tickets': 'maxCompTickets',  # noqa: E501
         'ticket_expiration_date_time': 'ticketExpirationDateTime',  # noqa: E501
+        'redemption_instructions': 'redemptionInstructions',  # noqa: E501
     }
 
     read_only_vars = {
@@ -195,6 +197,7 @@ class Performance(ModelNormal):
             payment_design_id (NanoID): [optional]  # noqa: E501
             max_comp_tickets (float): [optional]  # noqa: E501
             ticket_expiration_date_time (datetime): [optional]  # noqa: E501
+            redemption_instructions (str): [optional]  # noqa: E501
         """
 
         _check_type = kwargs.pop('_check_type', True)
@@ -306,6 +309,7 @@ class Performance(ModelNormal):
             payment_design_id (NanoID): [optional]  # noqa: E501
             max_comp_tickets (float): [optional]  # noqa: E501
             ticket_expiration_date_time (datetime): [optional]  # noqa: E501
+            redemption_instructions (str): [optional]  # noqa: E501
         """
 
         _check_type = kwargs.pop('_check_type', True)
