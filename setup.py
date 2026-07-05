7e8559bf25a1314124b17b9bbfa1a182aba0967c
johnturnham <support@wallet.inc>
2026-07-05 07:33:49 +0000

    v2.1.604

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 """
     wallet-api
 
-    Wallet Inc. API reference.  **Spec version 2.1.604**, built 2026-07-04T20:29:12.695Z
+    Wallet Inc. API reference.  **Spec version 2.1.604**, built 2026-07-05T07:29:54.586Z
 
     The version of the OpenAPI document: 2.1.604
     Contact: development@wallet.inc
@@ -45,7 +45,7 @@ setup(
     license="https://wallet.law",
     long_description_content_type='text/markdown',
     long_description="""\
-    Wallet Inc. API reference.  **Spec version 2.1.604**, built 2026-07-04T20:29:12.695Z
+    Wallet Inc. API reference.  **Spec version 2.1.604**, built 2026-07-05T07:29:54.586Z
     """,  # noqa: E501
     package_data={"wallet": ["py.typed"]},
 )
